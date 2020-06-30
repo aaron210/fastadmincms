5142849e557b0b1891ac686221ce6457219b0289
fastadmincms <fastcms@qq.com>
2020-06-30 09:52:36 +0800

    no commit message

---
+++ fastadmincms/My/Config/License.php
@@ -0,0 +1,13 @@
+<?php
+// 此文件是安装授权文件，每次下载安装包会自动生成，请勿修改
+return [
+
+    'oem' => '1',
+    'name' => '云南鸿鹄网络工作室',
+    'url' => 'http://www.fastadmincms.com',
+    'domain' => '',
+    'cloud' => '',
+    'service' => '',
+    'license' => 'C4CA4238A0B923820DCC509A6F75841',
+
+];
---
+++ fastadmincms/My/Config/Version.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'id' => '11',
+    'name' => 'FastAdminCMS',
+    'version' => '2.1',
+    'updatetime' => '2020-06-01',
+    'downtime' => '2020-06-28 17:09:06',
+
+];
